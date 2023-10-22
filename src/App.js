38db0632e0cf1faa755158f5417c778e80476a1e
mbahmu25 <nikogenta25@gmail.com>
2023-10-22 12:56:52 +0700

    tes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+var daftar = 10;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex">
+        <h1 className="bg-gray-300 text-3xl text-bold p-5 ">
+          Nicholas Genta Setiawan Gunawan
+          <br />
+        </h1>
+      </div>
+      <br />
+      <Tombol />
+      {daftar}
+      {/* {daftar.join(", ")} */}
     </div>
   );
 }
+export function Tombol() {
+  function warn() {
+    alert("Masuk");
+  }
+  const [i, hitung] = useState(0);
 
+  return (
+    <div className="border border-5 border-black rounded-[3px] w-fit mb-2">
+      <button className="bg-red-200" onClick={() => hitung(i * 2 + 1)}>
+        klik
+      </button>
+      <p>angka:{i}</p>
+    </div>
+  );
+}
+function Kotak() {
+  return (
+    <>
+      <div className="grid grid-col">
+
+      </div>
+    </>
+  );
+}
 export default App;
+// pelajari grid di tailwinds
+// peljari class components
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {Tombol} from './App'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Tombol />
+    <Tombol />
+    <Tombol />
   </React.StrictMode>
 );
 
