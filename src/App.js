e67f0f9a6063b6ed64ffb304f86652b7e2d578d0
mbahmu25 <nikogenta25@gmail.com>
2023-10-24 00:01:58 +0700

    benerin peta

--- src/App.js
+++ src/App.js
@@ -2,51 +2,36 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Line } from "react-chartjs-2";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
-import { MapContainer, TileLayer } from "react-leaflet";
 import Map from "./Map";
 var daftar = 10;
 
 function App() {
   const pos = [51.2, 51];
   return (
-    <div className="p-[20px]">
-      <Judul />
-      <div className="h-fit">
-        <div className="grid grid-col-2 gap-[20px] grid-flow-col">
-          <Map />
-          <Grafik teks="4" cs="4" />
-        </div>
-      </div>
-      <Kotak />
-    </div>
-  );
-}
-function Judul() {
-  return (
-    <>
-      <div className="App">
+    <div className="p-[20px] bg-red">
+      <div className="App mb-[20px]">
         <h1 className="text-3xl font-bold">PENUGASAN GMAT</h1>
         <h1 className="text-xl"> Nicholas Genta Setiawan Gunawan</h1>
       </div>
-    </>
-  );
-}
-function Kotak() {
-  return (
-    <>
+      <div className="grid grid-cols-12 gap-[20px] grid-flow-col">
+        <div className="grid bg-gray-100 rounded-md col-span-8 pr-5 pl-5 pt-5 pb-5  ">
+          <Map />
+        </div>
+        <div className="bg-gray-100 rounded-md col-span-4 h-fit ">
+          <Grafik3 teks="4" cs="5" />
+        </div>
+      </div>
       <div className="grid grid-cols-3 gap-[20px] grid-flow-col mt-[25px] ">
         <Grafik teks="1" cs="4  " />
         <Grafik teks="3" cs="4" />
         <Grafik teks="2" cs="4" />
       </div>
-    </>
+    </div>
   );
 }
 function Grafik({ teks, cs }) {
   const [datas, tambah] = useState([]);
   const [y, Yval] = useState([]);
-  const [i, hit] = useState(0);
   var data = [4, 1, 5, 1, 5, 3, 6, 7, 12, 6, 1, 5, 15, 2, 3];
   var angka = 0;
   useEffect(() => {
@@ -64,7 +49,7 @@ function Grafik({ teks, cs }) {
   }, []);
   console.log(data[0], y);
   return (
-    <div className={"border border-black rounded-md h-fit "}>
+    <div className={"rounded-md h-fit bg-gray-100 pr-5 pl-5 pt-5 pb-3"}>
       <Line
         data={{
           labels: y.slice(-10),
@@ -72,11 +57,64 @@ function Grafik({ teks, cs }) {
             {
               label: teks,
               data: datas.slice(-10),
+              tension: 0.4,
             },
           ],
-          options: {
-            bezierCurve: true,
-          },
+        }}
+      />
+    </div>
+  );
+}
+function Grafik3({ teks, cs }) {
+  const [datas, tambah] = useState([]);
+  const [datas2, tambah2] = useState([]);
+  const [datas3, tambah3] = useState([]);
+  const [y, Yval] = useState([]);
+  var data = [4, 1, 5, 1, 5, 3, 6, 7, 12, 6, 1, 5, 15, 2, 3];
+  var angka = 0;
+  var tensi = 0.2;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      var ran = Math.floor(Math.random() * 100);
+
+      tambah((old) => [...old, Math.floor(Math.random() * 100)]);
+      tambah2((old) => [...old, Math.floor(Math.random() * 100)]);
+      tambah3((old) => [...old, Math.floor(Math.random() * 100)]);
+      Yval((oldY) => [...oldY, angka++]);
+    }, 1000);
+
+    return () => {
+      // clear up
+      clearInterval(interval);
+    };
+  }, []);
+  console.log(data[0], y);
+  return (
+    <div className={"mr-5 ml-5 mt-5 mb-2"}>
+      <Line
+        data={{
+          labels: y.slice(-10),
+          datasets: [
+            {
+              label: teks,
+              data: datas.slice(-10),
+              borderColor: "red",
+              tension: tensi,
+            },
+            {
+              label: teks,
+              data: datas2.slice(-10),
+              borderColor: "yellow",
+              tension: tensi,
+            },
+            {
+              label: teks,
+              data: datas3.slice(-10),
+              borderColor: "blue",
+              tension: tensi,
+            },
+          ],
+          tension: 0.2,
         }}
       />
     </div>
--- src/Map.jsx
+++ src/Map.jsx
@@ -1,22 +1,43 @@
 import React from "react";
-import { MapContainer, TileLayer } from "react-leaflet";
- 
-export default function Map () {
-  const position = [8.1386, 5.1026]; // [latitude, longitude]
-  const zoomLevel = 13;
- 
+import "./App.css";
+import { useEffect, useState } from "react";
+import { MapContainer, Marker, TileLayer, Circle } from "react-leaflet";
+
+export default function Map() {
+  const zoomLevel = 11;
+  const [position, setPos] = useState([8.1386, 5.1026]);
+  const [ganti, setGanti] = useState(0.001);
+  // const [i, hit] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPos(() => [(position[0] += 0.001), (position[1] += 0.001)]);
+    }, 1000);
+
+    return () => {
+      // clear up
+      clearInterval(interval);
+    };
+  }, []);
   return (
-    <div className="col-span-8">
-        <MapContainer 
-            center={position} 
-            zoom={zoomLevel} 
-            scrollWheelZoom={false}
-        >
+    <>
+      <div>
+        <h1 className="text-xl">GPS</h1>latitude: {position[0].toFixed(4)} longitude:{" "}
+        {position[0].toFixed(4)}
+      </div>
+      <br />
+      <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={false}>
         <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        </MapContainer>
-    </div>
+        <Circle
+          center={position}
+          radius={80}
+          fillColor="red"
+          color="red"
+          className="bg-color-red"
+        />
+      </MapContainer>
+    </>
   );
-};
+}
